e02ff9afa39c285a970029a590043e4e2ee3e5a1
Sam POUZET <samuel.pouzet@yahoo.fr>
2022-08-07 16:02:39 +0200

    skeleton

---
+++ module/billing/config/module.config.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'controllers' => [
+        'factories' => [],
+    ],
+    'router' => [
+        'routes' => [],
+    ],
+    'api-tools-versioning' => [
+        'uri' => [],
+    ],
+    'api-tools-rpc' => [],
+    'api-tools-content-negotiation' => [
+        'controllers' => [],
+        'accept_whitelist' => [],
+        'content_type_whitelist' => [],
+    ],
+];
